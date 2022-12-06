ee043a27ce07570e1d2f8b0605eb252dcf4e0b78
stefcenti2022 <110138522+stefcenti2022@users.noreply.github.com>
2022-12-06 02:18:01 -0500

    Add usage instructions and troubleshooting info to readme.

--- README.md
+++ README.md
@@ -12,6 +12,31 @@ The data for this project, was obtained by scraping the following sites for imag
 - Mars hemispheres site:
   https://marshemispheres.com/
 
+## Usage
+
+1: Open a Terminal window and start the MongoDB server:
+
+    brew services start mongodb-community@5.0
+
+2: Check that the mars_app DB exists. If not, create it a new one. Start the mongo DB in the terminal by entering the command `mongo`. From the Mongo DB CLI enter `show databases`.
+
+2: Run the app:
+
+    python app.py
+
+3: The previous command will start the app on port 127.0.0.1:5000. Go to this port in your web browser to open the Mission to Mars site:
+
+  <img src="./images/landing_page.jpeg" alt="Mars Landing Page" width="500"/>
+
+4: When the 'Latest News' button is clicked, the '/scrape' route will be used to get the latest news and display it in the appropriate section on the webpage:
+
+  <img src="./images/latest_news.jpeg" alt="Mars News" width="500"/>
+
+5: Shut down the Mongo DB server using the following commands from the Mongo CLI:
+
+    > use admin
+    > db.shutdownServer()
+
 ## Folders/Files
 
 - templates: This folder contains the index.html template required to render the data from app.py.
@@ -46,3 +71,31 @@ The data for this project, was obtained by scraping the following sites for imag
   - mars_facts(): uses pandas to scrape the mars facts site for the latest facts storing it in a dataframe. This dataframe is converted to html to be returned as an pre-formatted table to be displayed neatly on the site
 
   - mars_hemispheres(browser): given a browser this function will visit the mars hemispheres site to retrieve the high resolution images and titles for each of mars 4 hemispheres. The hemispheres will be stored as a dictionary object with the attributes 'image_url' and 'title' returned as a list of dictionaries.
+
+  ## Troubleshooting Tips
+
+  ### Port in Use
+
+  To stop the app, use the terminal to run the following commands:
+
+  - `ps -fA | grep python`
+
+    Shows a list of python processess with their process ids.
+
+  - `kill -9 pid`
+
+    Where pid is the process id of app.py
+
+  ### To Check Data in Mongo DB
+
+  From the terminal enter `mongo` and enter the following commands from the mongo CLI:
+
+  ```
+  > use mars_app
+  > show collections
+  > db.<collection_name>.find()
+  ```
+
+  ### Mongo DB Process Documentation
+
+  https://www.mongodb.com/docs/manual/tutorial/manage-mongodb-processes/
--- scraping.py
+++ scraping.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 import pandas as pd
 import datetime as dt
+import re
 
 # Set up Splinter
 def scrape_all():
@@ -20,7 +21,8 @@ def scrape_all():
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now(),
-        "hemispheres": mars_hemispheres(browser)
+        "hemispheres": mars_hemispheres(browser),
+        "background_image": mars_travel_routes(browser)
     }
 
     # Stop webdriver and return data
@@ -124,58 +126,118 @@ def mars_hemispheres(browser):
 
     # Convert the browser html to a soup object
     html = browser.html
-
     title_soup = soup(html, 'html.parser')
 
-    # Titles are on the first page so get them first using the 'h3' tag.
-    # The parent of each title element will be used to get the html page for the image.
-    title_elems = title_soup.find_all('h3')
+    try:
+        # Titles are on the first page so get them first using the 'h3' tag.
+        # The parent of each title element will be used to get the html page for the image.
+        title_elems = title_soup.find_all('h3')
 
-    # 3. Write code to retrieve the image urls and titles for each hemisphere.
-    for title_elem in title_elems:
+        # 3. Write code to retrieve the image urls and titles for each hemisphere.
+        for title_elem in title_elems:
+            
+            # If title == 'Back' we are done with the list of titles.
+            # Alternatively, we could use the parent to get the last href = '#'
+            # We also need to skip elements that have blank titles.
+            if title_elem.text == 'Back':
+                break
+            elif title_elem.text == '':
+                continue
+
+            # Get the html page to visit in order to find the image
+            image_url = title_elem.parent['href']
+            
+            # Save the title
+            image_title = title_elem.text
+
+            # Get the full path to the html page
+            image_url = f"{url}{image_url}"
+            
+            # Go to the url where the image info is located:
+            browser.visit(image_url)
+            
+            # Convert the new html page to a soup object
+            html = browser.html
+            image_soup = soup(html, 'html.parser')
+
+            # Get the 'a' tag where it's text = 'Sample':
+            image_url = image_soup.find('a', text='Sample').get('href')
+
+            # Get use the href to get the full url to the enhanced image:
+            image_url = f"{url}{image_url}"
+
+            # Create a key value pair in the hemisphere variable:
+            hemisphere = {'image_url': image_url, 'title': image_title}
+
+            # Add the hemisphere to the list of hemispheres:
+            hemisphere_image_urls.append(hemisphere)
+
+            # Navigate back to the beginning to get the next hemisphere title and image.
+            browser.back()
         
-        # If title == 'Back' we are done with the list of titles.
-        # Alternatively, we could use the parent to get the last href = '#'
-        # We also need to skip elements that have blank titles.
-        if title_elem.text == 'Back':
-            break
-        elif title_elem.text == '':
-            continue
-
-        # Get the html page to visit in order to find the image
-        image_url = title_elem.parent['href']
-        
-        # Save the title
-        image_title = title_elem.text
-
-        # Get the full path to the html page
-        image_url = f"{url}{image_url}"
-        
-        # Go to the url where the image info is located:
-        browser.visit(image_url)
-        
-        # Convert the new html page to a soup object
-        html = browser.html
-        image_soup = soup(html, 'html.parser')
-
-        # Get the 'a' tag where it's text = 'Sample':
-        image_url = image_soup.find('a', text='Sample').get('href')
-
-        # Get use the href to get the full url to the enhanced image:
-        image_url = f"{url}{image_url}"
-
-        # Create a key value pair in the hemisphere variable:
-        hemisphere = {'image_url': image_url, 'title': image_title}
-
-        # Add the hemisphere to the list of hemispheres:
-        hemisphere_image_urls.append(hemisphere)
-
-        # Navigate back to the beginning to get the next hemisphere title and image.
-        browser.back()
+    except AttributeError:
+        return None
    
     # 4. Return the list that holds the dictionary of each image url and title.
     return hemisphere_image_urls
 
+# Find the first image of a travel route on mars.
+# These images have the word 'route' in their href.
+# Use regex to parse the hrefs and stop at the first one with the word 'route'.
+def mars_travel_routes(browser):
+
+    # Set default_url to a default image in case none are found.
+    default_url = "https://mars.nasa.gov/system/resources/detail_files/26992_PIA24923_MAIN-web.jpg"
+
+    # 1. Use browser to visit the URL 
+    url = 'https://nasa.gov/'
+
+    browser.visit(url)
+
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+    # Convert the browser html to a soup object
+    html = browser.html
+
+    travel_soup = soup(html, 'html.parser')
+
+    outer_container = ""
+
+    try:
+        # Find all divs containing image and description    
+        slide_elems = travel_soup.find_all('div.image_and_description_container')
+
+        # 3. Write code to retrieve the image urls and titles for each hemisphere.
+        for slide_elem in slide_elems:
+            
+            # Get the next 'a' tag and get the partial href to the current image.
+            image_href = slide_elem.find('a').get('href')
+
+            # We want the first image that has the word route in it's href path
+            # Use regex to find the word 'route'.
+            if re.match('route', image_href, flags=re.IGNORECASE):
+                outer_container = slide_elem
+                break
+            
+        # Make sure we found a container with 'route' in  it's href.
+        if outer_container == "":
+            return default_url
+        
+        # We should have a div with an image for our background.
+        # This is the outer container. We need to get to the list_image div
+        slide_elem = outer_container.find('div.list_image')
+
+        # Get the src for the first image tag
+        image_src = slide_elem.find('img').get('src')
+
+        image_url = f"{url}{image_src}"
+
+    except AttributeError:
+        return default_url
+    
+    # 4. Return the list that holds the dictionary of each image url and title.
+    return image_url
 
 if __name__ == "__main__":
     # If running as script, print scraped data
